d41199c394ca6ec36e31b27f261c3607db045de7
Soufai19 <151896909+Soufai19@users.noreply.github.com>
2024-01-21 17:03:32 -0500

    Delete udemy.h

--- udemy.h
+++
@@ -1,7 +0,0 @@
-#ifndef UDEMY_H_INCLUDED
-#define UDEMY_H_INCLUDED
-
-int cargarEmpleados(struct empleado empleados[100]);
-void guardarEmpleados(struct empleado empleados[100], int num);
-
-#endif // UDEMY_H_INCLUDED
